d2be4d43363017b3eaef24c1abf7e6de77140175
Matthias Meten <4442419-meten@users.noreply.gitlab.com>
2020-11-24 15:51:29 +0100

    Bot revival

---
+++ index.js
@@ -0,0 +1,86 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+var CronJob = require('cron').CronJob;
+const util = require('util')
+const jsdom = require("jsdom");
+const fs = require('fs')
+const streamPipeline = util.promisify(require('stream').pipeline)
+const client = new Discord.Client();
+const CONFIG = require('./config.json');
+const prefix = CONFIG.prefix;
+const cronInspireChannel = CONFIG.cronInspireChannel;
+
+var results
+
+var job = new CronJob(
+	'0 0 8 * * *',
+	function() {
+		cronInspire()
+	},
+	null,
+	true,
+	'Poland'
+);
+job.start();
+
+client.on('message', message => {
+    let msg = message.content.toUpperCase();
+    let sender = message.author;
+
+    if (msg === prefix + `AVATAR`) {
+        message.reply(message.author.avatarURL);
+    };
+
+    if (msg === prefix + 'PING') {
+        message.channel.send(`Pong!\n\`MSG: ${Date.now() - message.createdTimestamp}ms\`\n\`API: ${Math.round(client.ws.ping)}ms\``);
+    };
+
+    if (msg === prefix + `INSPIRUJ` || msg === prefix + `INSPIRACJA`) {
+        (async () => {
+            const response = await fetch('https://inspirobot.me/api?generate=true')
+            .then(res => res.text())
+            .then(text => message.channel.send(text));
+            
+        })();
+    };
+
+    if (msg === prefix + `KOTEŁ`) {
+        (async () => {
+            const response = await fetch('https://thiscatdoesnotexist.com/')
+            if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+            console.log(response.body);
+            await streamPipeline(response.body, fs.createWriteStream('./placeholder.jpg'));
+            const attachment = new Discord.MessageAttachment('./placeholder.jpg');
+            message.channel.send(attachment);
+            
+        })();
+    };
+
+    if (msg === prefix + `INICJUJ` || msg === prefix + `INICJACJA`) {
+        (async () => {
+            id = Math.floor(Math.random() * 100000);
+            const response = await fetch('http://www.thiswaifudoesnotexist.net/example-' + id + '.jpg')
+            if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+            await streamPipeline(response.body, fs.createWriteStream('./placeholder.jpg'));
+            const attachment = new Discord.MessageAttachment('./placeholder.jpg');
+            message.channel.send(attachment);
+        })();
+    };
+});
+
+async function cronInspire() {
+    const response = await fetch('https://inspirobot.me/api?generate=true')
+            .then(res => res.text())
+            .then(text => client.channels.cache.get(cronInspireChannel).send(text));
+}
+
+client.on('ready', () => {
+    client.user.setStatus('online');
+    client.user.setActivity("Adam hitting women", {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/meten_"
+      });
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(CONFIG.token);
