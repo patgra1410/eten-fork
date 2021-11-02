9a61b899d9aa1f4ae840a324fedce5d84b20c7bd
MasloMaslane <mmasiarz@wp.pl>
2021-11-02 20:30:31 +0100

    morning weather for multiple cities

--- index.js
+++ index.js
@@ -44,34 +44,42 @@ const dailyJob = new cron.CronJob(
       dzwonekChannel.send({ content: 'Dzienna inspiracja się wyjebała <@567054306688106496> :(' })
     }
     // Get weather report image and send. Or at least, try to.
-    try {
-      const result = await fetch('https://m.meteo.pl/warszawa/60')
-      if (!result.ok) throw new Error(`Unexpected response ${result.statusText}`)
-      const resultText = await result.text()
-      const imageRegex = /src="(https:\/\/www\.meteo\.pl\/um\/metco\/mgram_pict\.php\?ntype=0u&fdate=[0-9]+&row=406&col=250&lang=pl)"/g
-      const link = imageRegex.exec(resultText)[1]
-      const imgResult = await fetch(link, {
-        headers: {
-          Accept: 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
-          'Accept-Encoding': 'gzip, deflate, br',
-          'Accept-Language': 'en-GB,en;q=0.9',
-          Host: 'www.meteo.pl',
-          Referer: 'https://m.meteo.pl/',
-          'Sec-Fetch-Dest': 'image',
-          'Sec-Fetch-Mode': 'no-cors',
-          'Sec-Fetch-Site': 'same-site',
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
-        }
-      })
-      if (!imgResult.ok) throw new Error(`Unexpected response ${result.statusText}`)
-      await streamPipeline(imgResult.body, fs.createWriteStream('./data/weather.png'))
-      const img = await joinImages(['data/leg60.png', 'data/weather.png'], { direction: 'horizontal' })
-      await img.toFile('data/weatherFinal.png')
-      const weatherAttachment = new Discord.MessageAttachment('./data/weatherFinal.png')
-      dzwonekChannel.send({ files: [weatherAttachment] })
-    } catch (error) {
-      console.error(`Daily weather failed... ${error}`)
-      dzwonekChannel.send({ content: 'Dzienna pogoda się wyjebała <@567054306688106496> :(' })
+    for(var city of config.cronWeather)
+    {
+      try {
+        const result = await fetch(city.link)
+        if (!result.ok) throw new Error(`Unexpected response ${result.statusText}`)
+        const resultText = await result.text()
+        // const imageRegex = /src="(https:\/\/www\.meteo\.pl\/um\/metco\/mgram_pict\.php\?ntype=0u&fdate=[0-9]+&row=406&col=250&lang=pl)"/g
+        const imageRegex = /src="(https:\/\/www\.meteo\.pl\/um\/metco\/mgram_pict\.php\?ntype=0u&fdate=[0-9]+&row=[0-9]+&col=[0-9]+&lang=pl)"/g
+        const link = imageRegex.exec(resultText)[1]
+        const imgResult = await fetch(link, {
+          headers: {
+            Accept: 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-GB,en;q=0.9',
+            Host: 'www.meteo.pl',
+            Referer: 'https://m.meteo.pl/',
+            'Sec-Fetch-Dest': 'image',
+            'Sec-Fetch-Mode': 'no-cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+          }
+        })
+        if (!imgResult.ok) throw new Error(`Unexpected response ${result.statusText}`)
+        await streamPipeline(imgResult.body, fs.createWriteStream('./data/weather.png'))
+        const img = await joinImages(['data/leg60.png', 'data/weather.png'], { direction: 'horizontal' })
+        await img.toFile('data/weatherFinal.png')
+        const weatherAttachment = new Discord.MessageAttachment('./data/weatherFinal.png')
+
+        if(config.cronWeather.length==0)
+          dzwonekChannel.send({ files: [weatherAttachment] })
+        else
+          dzwonekChannel.send({ content: city.title+':', files: [weatherAttachment] })
+      } catch (error) {
+        console.error(`Daily weather failed... ${error}`)
+        dzwonekChannel.send({ content: 'Dzienna pogoda się wyjebała <@567054306688106496> :(' })
+      }
     }
   },
   null,
