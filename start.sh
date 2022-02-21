17f5385a1e82d3be7abc7850f3a3a789d7b0ad79
MasloMaslane <mmasiarz@wp.pl>
2022-02-21 15:06:14 +0100

    attachment hashing

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 node . # czy cośtam niewiem zastąp tą linijkę
+touch data/crushed
 curl -X POST `cat webhook-link` -H "Content-Type: application/json" --data-binary @- <<DATA
 {
   "content": "@everyone\n\nETEN HAS WORKED\n`cat ./data/uptime`\nDAYS WITHOUT AN ACCIDENT"
