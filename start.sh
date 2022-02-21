ca69b822bf7a21d6b76ce9540d759cee87ea5d0e
MasloMaslane <mmasiarz@wp.pl>
2022-02-21 15:29:23 +0100

    nie umiem pisac

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 node . # czy cośtam niewiem zastąp tą linijkę
-touch data/crushed
+touch data/crashed
 curl -X POST `cat webhook-link` -H "Content-Type: application/json" --data-binary @- <<DATA
 {
   "content": "@everyone\n\nETEN HAS WORKED\n`cat ./data/uptime`\nDAYS WITHOUT AN ACCIDENT"
