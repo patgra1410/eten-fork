d10c860017d9f1c3f614c8274ca0e6cb23eb475c
Meten <4442419-meten@users.noreply.gitlab.com>
2022-03-19 22:30:51 +0100

    hotfix

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ client.commands = new Discord.Collection();
 async function updateSlashCommands() {
 	const slashCommands = [];
 	// for javascript command files
-	const commandFiles: string[] = fs.readdirSync(`${__dirname}/commands`).filter((file: string) => file.endsWith(".js"));
+	const commandFiles: string[] = fs.readdirSync(`${__dirname}/commands`).filter((file: string) => (file.endsWith(".js") && !file.startsWith("ts_")));
 	for (const file of commandFiles) {
 		const command: SlashCommandFile = require(`${__dirname}/commands/${file}`);
 		client.commands.set(command.data.name, command);
@@ -42,14 +42,14 @@ async function updateSlashCommands() {
 			client.commands.set(command.aliases[alias], command);
 	}
 	// for typescript command files
-	const tsCommandFiles: string[] = fs.readdirSync(`${__dirname}/commands`).filter((file: string) => file.startsWith("ts_"));
-	for (const file of tsCommandFiles) {
-		const command: SlashCommandFile = await import(`${__dirname}/commands/${file}`);
-		client.commands.set(command.data.name, command);
-		slashCommands.push(command.data.toJSON());
-		for (const alias in command.aliases)
-			client.commands.set(command.aliases[alias], command);
-	}
+	// const tsCommandFiles: string[] = fs.readdirSync(`${__dirname}/commands`).filter((file: string) => file.startsWith("ts_"));
+	// for (const file of tsCommandFiles) {
+	// 	const command: SlashCommandFile = await import(`${__dirname}/commands/${file}`);
+	// 	client.commands.set(command.data.name, command);
+	// 	slashCommands.push(command.data.toJSON());
+	// 	for (const alias in command.aliases)
+	// 		client.commands.set(command.aliases[alias], command);
+	// }
 	await client.application?.commands.set(slashCommands);
 }
 
