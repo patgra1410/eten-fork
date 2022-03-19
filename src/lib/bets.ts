417534228358a116341a5691cb151431a96b6b7a
Meten <4442419-meten@users.noreply.gitlab.com>
2022-03-19 18:16:41 +0100

    Fix merge mistakes

--- src/lib/bets.ts
+++ src/lib/bets.ts
@@ -1,6 +1,7 @@
-import fs from 'fs'
-import Discord, { Client, TextChannel } from 'discord.js'
-import config from '../config.json'
+import fs from "fs";
+import Discord, { TextChannel } from "discord.js";
+import config from "../config.json";
+import { client } from "../index";
 
 interface IBets {
 	[user: string]: {
@@ -9,54 +10,54 @@ interface IBets {
 }
 
 export async function addTime(time: Date) {
-	const predictions = JSON.parse(fs.readFileSync('./data/predictions.json', 'utf-8'))
+	const predictions = JSON.parse(fs.readFileSync("./data/predictions.json", "utf-8"));
 	if (predictions.bets[time.getDay()] === undefined)
-		predictions.bets[time.getDay()] = []
-	predictions.bets[time.getDay()].push(time.getTime())
-	fs.writeFileSync('./data/predictions.json', JSON.stringify(predictions))
+		predictions.bets[time.getDay()] = [];
+	predictions.bets[time.getDay()].push(time.getTime());
+	fs.writeFileSync("./data/predictions.json", JSON.stringify(predictions));
 }
 
-export async function check(client: Client<boolean>) {
+export async function check() {
 	// eneabled?
 	if (!config.bets.eneabled)
-		return
+		return;
 
-	const date = new Date()
-	const now = date.getMilliseconds() + date.getSeconds() * 1000 + date.getMinutes() * 60 * 1000 + date.getHours() * 60 * 60 * 1000
+	const date = new Date();
+	const now = date.getMilliseconds() + date.getSeconds() * 1000 + date.getMinutes() * 60 * 1000 + date.getHours() * 60 * 60 * 1000;
 
-	const users = []
-	const bets: IBets = JSON.parse(fs.readFileSync('./data/bets.json', 'utf8'));
+	const users = [];
+	const bets: IBets = JSON.parse(fs.readFileSync("./data/bets.json", "utf8"));
 
 	if (Object.keys(bets).length == 0)
-		return
+		return;
 
 	for (const [user, time] of Object.entries(bets)) {
-		const diff = Math.abs(time.time - now)
+		const diff = Math.abs(time.time - now);
 		// let mili = parseInt(diff)
 		// parseInt jest dla stringów, czy nie chciałeś tu roundować czy coś idk? podieniłem na math.round
-		let mili = Math.round(diff);
-		let sec = Math.floor(mili / 1000)
-		let mins = Math.floor(sec / 60)
-		let hours = Math.floor(mins / 60)
+		const mili = Math.round(diff);
+		const sec = Math.floor(mili / 1000);
+		const mins = Math.floor(sec / 60);
+		const hours = Math.floor(mins / 60);
 		// Mam nadzieję, że to działa, nie wiem co to za czarna magia że skaczesz z inta na stringa na inta (porównanie) na stringa
-		const miliS = ('000' + (mili % 1000)).slice(-3)
-		const secS = ('00' + (sec % 60)).slice(-2)
-		const minsS = ('00' + (mins % 60)).slice(-2)
-		const hoursS = ('00' + (hours % 60)).slice(-2)
+		const miliS = ("000" + (mili % 1000)).slice(-3);
+		const secS = ("00" + (sec % 60)).slice(-2);
+		const minsS = ("00" + (mins % 60)).slice(-2);
+		const hoursS = ("00" + (hours % 60)).slice(-2);
 
-		let niceDiff = ''
+		let niceDiff = "";
 		if (time.time > now)
-			niceDiff = 'za pózno o '
+			niceDiff = "za pózno o ";
 		else
-			niceDiff = 'za wcześnie o '
+			niceDiff = "za wcześnie o ";
 		if (hours > 0)
-			niceDiff += `${hoursS}:${minsS} godzin`
+			niceDiff += `${hoursS}:${minsS} godzin`;
 		else if (mins > 0)
-			niceDiff += `${minsS} minut`
+			niceDiff += `${minsS} minut`;
 		else if (sec > 0)
-			niceDiff += `${secS} sekund`
+			niceDiff += `${secS} sekund`;
 		else if (mili > 0)
-			niceDiff += `${miliS} milisekund`
+			niceDiff += `${miliS} milisekund`;
 
 		users.push({
 			user: user,
@@ -64,34 +65,34 @@ export async function check(client: Client<boolean>) {
 			message: time.message,
 			diff: diff,
 			niceDiff: niceDiff
-		})
+		});
 	}
 
 	users.sort((a, b) => {
-		return a.diff - b.diff
-	})
+		return a.diff - b.diff;
+	});
 
-	let desc = ''
-	let i = 1
+	let desc = "";
+	let i = 1;
 	for (const user of users) {
-		desc += `**${i.toString()}.** <@${user.user}>: ${user.niceDiff} (podany czas: ${user.message})\n`
-		i++
+		desc += `**${i.toString()}.** <@${user.user}>: ${user.niceDiff} (podany czas: ${user.message})\n`;
+		i++;
 	}
 
 	const embed = new Discord.MessageEmbed()
 		.setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`)
-		.setTitle('Dzisiejsze wyniki zakładuw')
+		.setTitle("Dzisiejsze wyniki zakładuw")
 		.setDescription(desc);
 
 	(client.guilds.cache.get(config.bets.guild).channels.cache.get(config.bets.channel) as TextChannel).send({ embeds: [embed] });
 
-	fs.writeFileSync('./data/bets.json', '{}');
+	fs.writeFileSync("./data/bets.json", "{}");
 
-	const ranking = JSON.parse(fs.readFileSync('./data/ranking.json', 'utf-8'));
+	const ranking = JSON.parse(fs.readFileSync("./data/ranking.json", "utf-8"));
 
 	if (!(users[0].user in ranking.bets))
-		ranking.bets[users[0].user] = 0
-	ranking.bets[users[0].user]++
+		ranking.bets[users[0].user] = 0;
+	ranking.bets[users[0].user]++;
 
-	fs.writeFileSync('./data/ranking.json', JSON.stringify(ranking))
+	fs.writeFileSync("./data/ranking.json", JSON.stringify(ranking));
 }
--- src/lib/discordEvents/reactionRemove.ts
+++ src/lib/discordEvents/reactionRemove.ts
@@ -1,25 +1,25 @@
-import { MessageReaction, User } from "discord.js"
+import { MessageReaction, User } from "discord.js";
 
 export default async function(reaction: MessageReaction, reactedUser: User) {
 	try {
-		const letters = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮']
-		const letter = String.fromCharCode(letters.indexOf(reaction.emoji.name) + 'a'.charCodeAt(0))
-		const user = reaction.message.guild.members.cache.find(member => member.id == reactedUser.id)
+		const letters = ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮"];
+		const letter = String.fromCharCode(letters.indexOf(reaction.emoji.name) + "a".charCodeAt(0));
+		const user = reaction.message.guild.members.cache.find(member => member.id == reactedUser.id);
 
-		if (reaction.message.id == '932695585465704448') // 1. klasa
-			await user.roles.remove(user.guild.roles.cache.find(role => role.name == `1${letter}`))
-		else if (reaction.message.id == '932695586426196060') // 2. klasa
-			await user.roles.remove(user.guild.roles.cache.find(role => role.name == `2${letter}`))
-		else if (reaction.message.id == '932695588540141598') // 3. klasa podstawowka
-			await user.roles.remove(user.guild.roles.cache.find(role => role.name == `3${letter}4`))
-		else if (reaction.message.id == '932695587424444466') // 3. klasa gimnazjum
-			await user.roles.remove(user.guild.roles.cache.find(role => role.name == `3${letter.toUpperCase()}3`))
+		if (reaction.message.id == "932695585465704448") // 1. klasa
+			await user.roles.remove(user.guild.roles.cache.find(role => role.name == `1${letter}`));
+		else if (reaction.message.id == "932695586426196060") // 2. klasa
+			await user.roles.remove(user.guild.roles.cache.find(role => role.name == `2${letter}`));
+		else if (reaction.message.id == "932695588540141598") // 3. klasa podstawowka
+			await user.roles.remove(user.guild.roles.cache.find(role => role.name == `3${letter}4`));
+		else if (reaction.message.id == "932695587424444466") // 3. klasa gimnazjum
+			await user.roles.remove(user.guild.roles.cache.find(role => role.name == `3${letter.toUpperCase()}3`));
 
 	}
 	catch (except) {
-		console.log(reaction)
-		console.log(except)
-		const user = reaction.message.guild.members.cache.find(member => member.id == reactedUser.id)
-		user.send('Był problem z usunięciem twojej roli, spróbuj jeszcze raz.')
+		console.log(reaction);
+		console.log(except);
+		const user = reaction.message.guild.members.cache.find(member => member.id == reactedUser.id);
+		user.send("Był problem z usunięciem twojej roli, spróbuj jeszcze raz.");
 	}
 }
