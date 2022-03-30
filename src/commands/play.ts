4db93e22e64d7c37ae76f06f175690c4b002c5c4
MasloMaslane <mmasiarz@wp.pl>
2022-03-30 20:44:12 +0200

    /play random and some other stuff

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -21,8 +21,26 @@ export const data = new SlashCommandBuilder()
 				new SlashCommandIntegerOption()
 					.setName("numer")
 					.setDescription("Numer dźwięku (/play list)")
+					.setMinValue(1)
 					.setRequired(true)
 			)
+			.addIntegerOption(
+				new SlashCommandIntegerOption()
+					.setName("repeat")
+					.setDescription("Ile razy puścić dźwięk (defaultowo 1)")
+					.setMinValue(1)
+					.setRequired(false)
+			)
+	)
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName("random")	
+			.setDescription("Puść losowy dźwięk")
+	)
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName("stop")	
+			.setDescription("Zatrzymaj puszczanie dźwięku")
 	)
 	.addSubcommand(subcommand =>
 		subcommand
@@ -47,16 +65,22 @@ export async function execute(interaction: CommandInteraction) {
 
 		interaction.reply({ embeds: [embed] });
 	}
-	else if (interaction.options.getSubcommand() == "sound") {
-		const num = interaction.options.getInteger("numer");
+	else if (interaction.options.getSubcommand() == "sound" || interaction.options.getSubcommand() == "random") {
 		const files = fs.readdirSync("./soundeffects");
+		let num, repeat = interaction.options.getInteger("repeat") ? interaction.options.getInteger("repeat") : 1;
 
+		if (interaction.options.getSubcommand() == "sound")
+			num = interaction.options.getInteger("numer");
+		else
+			num = Math.floor(Math.random() * files.length) + 1;
+
+			
 		if (num < 1 || num > files.length) {
-			interaction.reply("Nie poprawny numer");
+			interaction.reply("Niepoprawny numer");
 			return;
 		}
-		const fileName = files[num - 1];
 
+		const fileName = files[num - 1];
 		const guild = interaction.client.guilds.cache.get(interaction.guildId);
 		const user = guild.members.cache.get(interaction.user.id);
 
@@ -65,7 +89,8 @@ export async function execute(interaction: CommandInteraction) {
 			return;
 		}
 
-		interaction.reply(`Puszczanie dźwięku ${fileName}`);
+		const additionalText = repeat > 1 ? ` ${repeat} razy.` : "";
+		interaction.reply(`Puszczanie dźwięku ${fileName}${additionalText}`);
 
 		const channel = user.voice.channel;
 		const connection = joinVoiceChannel({
@@ -73,11 +98,33 @@ export async function execute(interaction: CommandInteraction) {
 			guildId: channel.guild.id,
 			adapterCreator: channel.guild.voiceAdapterCreator
 		});
-		const resource = createAudioResource(`./soundeffects/${fileName}`);
 		connection.subscribe(player);
-		player.play(resource);
-		while (player.state.status != "idle")
-			await sleep(100);
+
+		for (let i = 1; i <= repeat; i++) {
+			const resource = createAudioResource(`./soundeffects/${fileName}`);
+			player.play(resource);
+			while (player.state.status != "idle")
+				await sleep(100);
+		}
 		connection.disconnect();
+	}
+	else if (interaction.options.getSubcommand() == "stop") {
+		const guild = interaction.client.guilds.cache.get(interaction.guildId);
+		const user = guild.members.cache.get(interaction.user.id);
+
+		if (!user.voice.channel) {
+			interaction.reply("Nie jesteś na VC na tym serwerze");
+			return;
+		}
+
+		if (!guild.me.voice.channel) {
+			interaction.reply("Bot nie jest na VC");
+			return;
+		}
+
+		player.stop();
+		guild.me.voice.disconnect();
+
+		interaction.reply("Rozłączono");
 	}
 }
